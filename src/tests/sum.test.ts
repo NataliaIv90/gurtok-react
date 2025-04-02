5463b335005c27745f349d212b1064eb8293a442
Nataliia Ivantsova <nataliaivantsova@waterford.org>
2025-04-02 19:42:32 +0300

    removed husky

--- src/tests/sum.test.ts
+++ src/tests/sum.test.ts
@@ -1,12 +1,12 @@
-import { test, expect, assert, describe } from "vitest";
+import { test, expect, assert, describe } from 'vitest';
 
-import { sum } from "@/tests/sum.ts";
+import { sum } from '@/tests/sum.ts';
 
-describe("Sum function tests", () => {
-  test("adds 1 + 2 to equal 3", () => {
+describe('Sum function tests', () => {
+  test('adds 1 + 2 to equal 3', () => {
     expect(sum(1, 2)).to.equal(3);
   });
-  test("adds 2 + 3 to equal 5", () => {
+  test('adds 2 + 3 to equal 5', () => {
     assert.equal(sum(2, 3), 5);
   });
 });
